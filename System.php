3394b650c46e19efb742a1c9f1ff467278a6ca4f
Usanter <trolland@protonmail.ch>
2017-01-26 10:05:23 +0100

    First file

---
+++ System.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name:Système de recommandation en fonction de critères
+Plugin URI: https://github.com/Usanter/Systeme-de-recommandation-de-Live-Escape-Game
+Description: Un plugin de recommandation multicritères statique
+Version: 0.1
+Author: WeAreNotAlone
+License: GPL2
+*/
